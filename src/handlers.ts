dc802e62c10da4b24821efe499e93b49a479eab6
Aleksandr <a13x.student@yandex.ru>
2022-01-13 14:49:26 +0300

    delete runApp comment

--- src/handlers.ts
+++ src/handlers.ts
@@ -5,11 +5,7 @@ import assert from 'assert';
 require('dotenv').config()
 
 
-export const runAppHandler: ScenarioHandler = ({ req, res }) => {
-    // const keyset = req.i18n(dictionary)
-    // const responseText = keyset('Привет')
-    // res.appendBubble(responseText)
-    // res.setPronounceText(responseText)
+export const runAppHandler: ScenarioHandler = () => {
 }
 
 export const noMatchHandler: ScenarioHandler = async ({ req, res }) => {
